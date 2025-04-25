5ec7484b1aedcbc8e94ef349253ac61024ce0c66
Ritrix <ryanpauloneill@googlemail.com>
2025-04-25 20:32:14 +0100

    movement working and level design partial

--- My project/Assets/Scripts/playerController.cs
+++ My project/Assets/Scripts/playerController.cs
@@ -3,19 +3,37 @@ using UnityEngine.InputSystem;
 
 public class playerController : MonoBehaviour
 {
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D playerRigidBody;
     public InputAction rightAction;
     public InputAction leftAction;
+    private int horizontal;
+    Vector2 move;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rigidbody2D.GetComponent<Rigidbody2D>();
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        leftAction.Enable();
+        rightAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rightAction.IsPressed())
+        {
+            move = Vector2.right;
+        }
+        else if (leftAction.IsPressed())
+        {
+            move = Vector2.left;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
+        playerRigidBody.MovePosition(position);
     }
 }
