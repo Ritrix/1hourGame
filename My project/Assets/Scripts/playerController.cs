dec445b2819149d73088a811f266a3b6a1d90e92
Ritrix <ryanpauloneill@googlemail.com>
2025-04-25 21:32:05 +0100

    added adjustable jump, added character flipping

--- My project/Assets/Scripts/playerController.cs
+++ My project/Assets/Scripts/playerController.cs
@@ -6,8 +6,11 @@ public class playerController : MonoBehaviour
     Rigidbody2D playerRigidBody;
     public InputAction rightAction;
     public InputAction leftAction;
-    private int horizontal;
-    Vector2 move;
+    public InputAction upAction;
+    public float speed = 3.0f;
+    public float jumpVel = 10.0f;
+    private float horizontal = 1;
+    bool grounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,25 +18,63 @@ public class playerController : MonoBehaviour
         playerRigidBody = GetComponent<Rigidbody2D>();
         leftAction.Enable();
         rightAction.Enable();
+        upAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // left right movement
         if (rightAction.IsPressed())
         {
-            move = Vector2.right;
+            playerRigidBody.linearVelocity = new Vector2(speed, playerRigidBody.linearVelocityY);
+            horizontal = 1;
         }
         else if (leftAction.IsPressed())
         {
-            move = Vector2.left;
+            playerRigidBody.linearVelocity = new Vector2(-speed, playerRigidBody.linearVelocityY);
+            horizontal = -1;
         }
+        else
+        {
+            // stop player when not inputting left and right
+            playerRigidBody.linearVelocity = new Vector2(0, playerRigidBody.linearVelocityY);
+        }
+
+        // jump
+        if (upAction.IsPressed())
+        {
+            if (grounded)
+            {
+                playerRigidBody.linearVelocity = new Vector2(playerRigidBody.linearVelocityX, jumpVel);
+                grounded = false;
+            }
+        }
+        // controllable jump height
+        if (upAction.WasReleasedThisFrame())
+        {
+            playerRigidBody.linearVelocity = new Vector2(playerRigidBody.linearVelocityX, playerRigidBody.linearVelocityY / 2);
+        }
+
+        // flip character
+        if (horizontal > 0.1f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if(horizontal < -0.1f)
+        {
+            transform.transform.localScale = new Vector3(-1,1,1);
+        }
+
+        
     }
 
-    private void FixedUpdate()
+    // Checking if grounded
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-        playerRigidBody.MovePosition(position);
+        if(collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
     }
 }
